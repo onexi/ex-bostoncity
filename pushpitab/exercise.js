d4de67bc139a27c3537f4f5d0d04a564670124f5
pushpitab <pushpita@mit.edu>
2018-02-06 20:41:12 -0500

    Updated exercise file(s)

--- pushpitab/exercise.js
+++ pushpitab/exercise.js
@@ -3,9 +3,16 @@ var exercise = {};
 exercise.maxSalary = function() {
     var currentMax = 0.0;
     var dataLength = exercise.data.data.length;
+    var currentSal;
     for (var i = 0; i < dataLength; i++) {
         // update current salary
         // store in currentMax if it is greater than currentMax
+       // var curentMax= exercise.data[i] 
+
+       currentSal = Number(exercise.data.data[i][18]);
+       if (currentMax < currentSal) {
+           currentMax = currentSal;
+       }
     }
 
     return currentMax;
