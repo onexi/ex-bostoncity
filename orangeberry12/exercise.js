cb32a55623566d2176ad7cd3e3699ff883e2ebae
orangeberry12 <MyGitHubPw0rd>
2018-02-20 10:29:40 -0500

    Updated exercise file(s)

--- orangeberry12/exercise.js
+++ orangeberry12/exercise.js
@@ -22,6 +22,10 @@ exercise.salariesAbove = function(salary) {
     for (var i = 0; i < dataLength; i++) {
         // check if currentSal exceed salary
         // increment num_salaries
+        currentSal = exercise.data.data.[i][18];
+        if (currentSal > salary){
+            num_salaries++;
+        }
     }
     return num_salaries;
 };
