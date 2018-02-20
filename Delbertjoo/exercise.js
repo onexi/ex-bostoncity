29c11af4587944412f8c20defbdceb55f73ea6a7
Delbertjoo <dsjoo@mit.edu>
2018-02-20 10:34:44 -0500

    Updated exercise file(s)

--- Delbertjoo/exercise.js
+++ Delbertjoo/exercise.js
@@ -6,8 +6,11 @@ exercise.maxSalary = function() {
     for (var i = 0; i < dataLength; i++) {
         // update current salary
         // store in currentMax if it is greater than currentMax
+        if(Number(exercise.data.data[i][18]) > currentMax)
+        {
+            currentMax = Number(exercise.data.data[i][18])
+        };
     }
-
     return currentMax;
 };
 
@@ -18,6 +21,11 @@ exercise.salariesAbove = function(salary) {
     for (var i = 0; i < dataLength; i++) {
         // check if currentSal exceed salary
         // increment num_salaries
+        if(Number(exercise.data.data[i][18]) > 150000)
+        {
+            num_salaries += 1
+
+        };
     }
     return num_salaries;
 };
