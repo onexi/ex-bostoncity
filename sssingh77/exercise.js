fbd541cbfd3a6c58f6b20751e6c1812466ebb81d
sssingh77 <sssingh@mit.edu>
2018-02-20 10:29:17 -0500

    Updated exercise file(s)

---
+++ sssingh77/exercise.js
@@ -0,0 +1,31 @@
+var exercise = {};
+// calculate the maximum salary
+exercise.maxSalary = function() {
+    var currentMax = 0.0;
+    var dataLength = exercise.data.data.length;
+    for (var i = 0; i < dataLength; i++) {
+        // update current salary
+        if (Number(exercise.data.data[i][18]) > currentMax)
+        {
+            currentMax = exercise.data.data[i][18];
+        }
+        // store in currentMax if it is greater than currentMax
+    }
+
+    return currentMax;
+};
+
+exercise.salariesAbove = function(salary) {
+    var num_salaries = 0;
+    var dataLength = exercise.data.data.length;
+    var currentSal = 0;
+    for (var i = 0; i < dataLength; i++) {
+        // check if currentSal exceed salary
+        currentSal = exercise.data.data[i][18];
+        if (currentSal > 150000) {
+            num_salaries += 1;
+        } 
+        // increment num_salaries
+    }
+    return num_salaries;
+};
