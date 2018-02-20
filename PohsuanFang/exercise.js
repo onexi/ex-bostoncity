7e646b361bc40ab49df4e2a5ed09bbf1e5ce4b95
PohsuanFang <phfang@mit.edu>
2018-02-20 10:34:59 -0500

    Updated exercise file(s)

--- PohsuanFang/exercise.js
+++ PohsuanFang/exercise.js
@@ -4,21 +4,24 @@ exercise.maxSalary = function() {
     var currentMax = 0.0;
     var dataLength = exercise.data.data.length;
     for (var i = 0; i < dataLength; i++) {
-        if (Number(exercise.data[i][18])>currentMax)
-        {currentMax=Number(exercise.data[i][18]);};
+        if (Number(exercise.data.data[i][18])>currentMax)
+        {currentMax=Number(exercise.data.data[i][18]);};
     
         // update current salary
         // store in currentMax if it is greater than currentMax
-    }
+        }
 
     return currentMax;
-};
+    };
 
 exercise.salariesAbove = function(salary) {
     var num_salaries = 0;
     var dataLength = exercise.data.data.length;
     var currentSal = 0;
     for (var i = 0; i < dataLength; i++) {
+        currentSal=Number(exercise.data.data[i][18])
+        if (currentSal > salary)
+        {num_salaries=num_salaries+1};
         // check if currentSal exceed salary
         // increment num_salaries
     }
