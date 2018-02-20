22c2b14d1cf408aabfde4acfa1c3d805b2578b2e
OlamideOladeji <olamideoladeji@gmail.com>
2018-02-20 10:32:09 -0500

    Updated exercise file(s)

--- OlamideOladeji/exercise.js
+++ OlamideOladeji/exercise.js
@@ -20,7 +20,7 @@ exercise.salariesAbove = function(salary) {
     var dataLength = exercise.data.data.length;
     var currentSal = 0;
     for (var i = 0; i < dataLength; i++) {
-        currentSal = Number(exercise.data.data[i][18]);
+        currentSal = Number(exercise.data.data[i][15]);
         if (currentSal > salary) {
             num_salaries+=1;
         }
