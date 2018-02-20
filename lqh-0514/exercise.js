c6b4ad457a1d6645be0c11f3a945771804814c35
lqh-0514 <qianhuil@mit.edu>
2018-02-20 10:30:56 -0500

    Updated exercise file(s)

--- lqh-0514/exercise.js
+++ lqh-0514/exercise.js
@@ -4,6 +4,10 @@ exercise.maxSalary = function() {
     var currentMax = 0.0;
     var dataLength = exercise.data.data.length;
     for (var i = 0; i < dataLength; i++) {
+        if(Number(exercise.data.data[i][18]) > currentMax);
+            currentMax = Number(data.data[i][18]);
+
+    
         // update current salary
         // store in currentMax if it is greater than currentMax
     }
@@ -16,6 +20,9 @@ exercise.salariesAbove = function(salary) {
     var dataLength = exercise.data.data.length;
     var currentSal = 0;
     for (var i = 0; i < dataLength; i++) {
+        if(Number(exercise.data.data[i][18] > 150000));
+            num_salaries += 1
+
         // check if currentSal exceed salary
         // increment num_salaries
     }
